e89868bbee8772a8b78bcba8348b7b99eaee2231
seriousdev-gh <borisdrovnin@gmail.com>
2026-01-11 23:52:32 +0300

    add rubocop

--- 2025/1/main.rb
+++ 2025/1/main.rb
@@ -5,6 +5,6 @@ puts File.read('2025/1/input')
          .split
          .map(&:to_i)
          .each_with_object({ a: 50, r: 0 }) {
-  _2[:a] = (_2[:a] + _1) % 100
-  _2[:r] += 1 if _2[:a].zero?
+           _2[:a] = (_2[:a] + _1) % 100
+           _2[:r] += 1 if _2[:a].zero?
 }
